f1510fe457f7c04f1290cdc15c0cf7e799e1726a
Klauno <oliverioclau@gmail.com>
2026-06-01 01:08:25 -0300

    Fix Firebase configuration and restore working backend state

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,4 +1,3 @@
-// src/config/firebase.js
 import admin from "firebase-admin";
 import fs from "fs";
 import path from "path";
@@ -7,16 +6,18 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const serviceAccount = JSON.parse(
-    fs.readFileSync(
-        path.join(__dirname, "serviceAccountKey.json"),
-        "utf8"
-    )
+const serviceAccountRaw = fs.readFileSync(
+    path.join(__dirname, "serviceAccountKey.json"),
+    "utf8"
 );
+
+const serviceAccount = JSON.parse(serviceAccountRaw);
+
+
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
 
 export const db = admin.firestore();
-
